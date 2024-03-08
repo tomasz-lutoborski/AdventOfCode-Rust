a6e575629118ee23274213b983dce736749e59c9
Tomasz Lutoborski <tomasz@lutoborski.net>
2024-03-08 11:47:12 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,4 @@
+use aoc_rust::year2015;
+fn main() {
+    year2015::day1::solve();
+}
---
+++ src/year2015/day1.rs
@@ -0,0 +1,30 @@
+use std::{error, fs::read_to_string};
+
+fn parse_char(acc: i64, c: char) -> i64 {
+    match c {
+        '(' => acc + 1,
+        ')' => acc - 1,
+        _ => panic!("wrong input char"),
+    }
+}
+
+fn find_basement(input: String) -> usize {
+    let mut current_level = 0;
+    for c in input.char_indices() {
+        current_level = parse_char(current_level, c.1);
+        if current_level < 0 {
+            return c.0;
+        };
+    }
+
+    0
+}
+
+fn parse_inst(input: String) -> i64 {
+    input.chars().fold(0, parse_char)
+}
+
+pub fn solve() {
+    let file_content = read_to_string("inputs/Year2015/Day1.txt").unwrap();
+    println!("{}", find_basement(file_content));
+}
